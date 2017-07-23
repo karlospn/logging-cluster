8a670256746f352feacacf1bbf77b667cf4b08bc
Carlos Pons <karlospn@gmail.com>
2017-07-23 19:47:15 +0200

    Create default MVC with serilog elasticsearch sink installed

---
+++ Logging.Elk/Logging.Elk.WebAppWithSerilog/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Logging.Elk.WebAppWithSerilog.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _log;
+
+        public HomeController(ILogger<HomeController> log)
+        {
+            _log = log;
+        }
+        public IActionResult Index()
+        {
+            _log.LogInformation("Accessing {0}", new { Control = "HomeController", Action = "Index" });
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
